62acdb7ccdb7729709e4d70419f4c36e0970ea66
etctera <yilonglei790@gmail.com>
2023-11-12 14:31:06 +0800

    feat: lint-staged

--- nest-jwt/src/main.ts
+++ nest-jwt/src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import * as session from 'express-session'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.use(
+    session({
+      secret: 'etc',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  )
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
