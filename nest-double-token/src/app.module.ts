fbe4da8987e279f5e279bedd9f802963a8e71086
etctera <yilonglei790@gmail.com>
2023-11-16 22:37:42 +0800

    feat: finish double-token

--- nest-double-token/src/app.module.ts
+++ nest-double-token/src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { User } from './user/entities/user.entity'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: 'oor',
+      global: true,
+      signOptions: {
+        expiresIn: '30m',
+      },
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'etc123456',
+      database: 'double-token',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+      poolSize: 10,
+      connectorPackage: 'mysql2',
+      extra: {
+        authPlugin: 'sha256_password',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
